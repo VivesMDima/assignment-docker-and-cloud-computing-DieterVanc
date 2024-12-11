793b480cd4e4aed84578dc5c6e0c72d16ebc1ca9
Dieter Vancoillie <dieter.vancoillie@student.vives.be>
2024-12-11 18:11:57 +0100

    Docker compose werkt

--- backend_apitizers/src/main/java/ti/vives/be/apitizers/ApitizersApplication.java
+++ backend_apitizers/src/main/java/ti/vives/be/apitizers/ApitizersApplication.java
@@ -11,10 +11,12 @@ import java.io.IOException;
 public class ApitizersApplication {
 
 	public static void main(String[] args) {
+
 		Dotenv dotenv = Dotenv.load();
 		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
 		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
 		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		SpringApplication.run(ApiTizersApplication.class, args);
 
 		SpringApplication.run(ApitizersApplication.class, args);
 		try {
